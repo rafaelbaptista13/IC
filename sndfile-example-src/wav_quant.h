db747b040c6846e75cd08511bdc1375551cb23f1
DiogoCarvalhoo <dfac@ua.pt>
2022-10-28 11:23:24 +0100

    added argument for version selection on wav_quant

--- sndfile-example-src/wav_quant.h
+++ sndfile-example-src/wav_quant.h
@@ -10,46 +10,49 @@ class WAVQuant {
 	std::map<short, int> approximationMap;
 
   public:
-	WAVQuant(const int num_bits) {
+	WAVQuant(const int num_bits, const int version) {
         int interval = pow(2, (16-num_bits));
 
-        /*  Version of keeping the 0  (-8 -4 0 4) 
-        int counter = 0;  
-        int currentThreshold = -32768;
-        for (int i = -32768; i<= 32767; i++) {
-            if (i % interval == 0) { 
-                currentThreshold = i;    
-                counter = 0;
+        if (version == 2) {
+            /*  Version of keeping the 0  (-8 -4 0 4) */
+            int counter = 0;  
+            int currentThreshold = -32768;
+            for (int i = -32768; i<= 32767; i++) {
+                if (i % interval == 0) { 
+                    currentThreshold = i;    
+                    counter = 0;
+                }
+                // If the current value is between two multiples of interval value, change the currentThreshold to the next multiple
+                if (counter == (interval/2) + 1) {
+                    // If to make sure that the new threshold is inside the range of amplitudes
+                    // (If the new threshold is greater than the max amplitude, the threshold needs to remain the same)
+                    if (currentThreshold + interval < 32767) 
+                        currentThreshold = currentThreshold + interval;
+                }
+                approximationMap[i] = currentThreshold;
+                counter++;
+                
+                //std::cout << i << "\t" << currentThreshold << '\n';
             }
-            // If the current value is between two multiples of interval value, change the currentThreshold to the next multiple
-            if (counter == (interval/2) + 1) {
-                // If to make sure that the new threshold is inside the range of amplitudes
-                // (If the new threshold is greater than the max amplitude, the threshold needs to remain the same)
-                if (currentThreshold + interval < 32767) 
-                    currentThreshold = currentThreshold + interval;
-            }
-            approximationMap[i] = currentThreshold;
-            counter++;
-            
-            //std::cout << i << "\t" << currentThreshold << '\n';
-        }*/
+        }
         
-
-        /* Version of not keeping the 0   (-6 -2 2 6) */
-        int counter = 0;  
-        int currentThreshold = -32768 + interval / 2;
-        for (int i = -32768; i<= 32767; i++) {
-            // If the current value is in the middle of two multiples(of the interval value)
-            if (abs(i % interval) == (interval / 2)) { 
-                currentThreshold = i;    
-                counter = 0;
+        if (version == 1) {
+            /* Version of not keeping the 0   (-6 -2 2 6) */
+            int counter = 0;  
+            int currentThreshold = -32768 + interval / 2;
+            for (int i = -32768; i<= 32767; i++) {
+                // If the current value is in the middle of two multiples(of the interval value)
+                if (abs(i % interval) == (interval / 2)) { 
+                    currentThreshold = i;    
+                    counter = 0;
+                }
+                if (counter == (interval/2)) {
+                    currentThreshold = currentThreshold + interval;
+                }
+                approximationMap[i] = currentThreshold;
+                counter++;
+                
             }
-            if (counter == (interval/2)) {
-                currentThreshold = currentThreshold + interval;
-            }
-            approximationMap[i] = currentThreshold;
-            counter++;
-            
         }
         
 	}
@@ -61,11 +64,6 @@ class WAVQuant {
 
             samples[index] = approximationMap[samples[index]];
         }
-        /*
-        for(auto i = 0; i<samples.size(); i++) {
-            samples[i] = approximationMap[samples[i]];
-        }
-        */
         return samples;
 	}
 };
